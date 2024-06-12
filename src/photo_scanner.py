6d34d3bd3984aac7f812e74ac7e8220336c19824
Kosta404 <kgorskiy07@gmail.com>
2024-06-12 21:30:49 +0100

    Added TextExtractor class
    * Implemented text extraction and parsing mechanism.
    * Added TODOs to the list

--- src/photo_scanner.py
+++ src/photo_scanner.py
@@ -1,9 +1,9 @@
 import cv2
 import numpy as np
 import os
-import easyocr
 from shutil import rmtree, move
 from datetime import datetime
+import pytesseract
 
 
 class FileProcessor:
@@ -89,10 +89,11 @@ class PhotoScanner:
     """
 
     def __init__(self):
-        self.reader = easyocr.Reader(['pt'])
+        pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
         self.file_worker = FileProcessor()
 
-    def enhance_photo(self, path_to_photo):
+    @staticmethod
+    def enhance_photo(path_to_photo):
         """
         Enhance symbols visibility on the photo
 
@@ -113,7 +114,7 @@ class PhotoScanner:
         eroded = cv2.erode(denoised, kernel, iterations=1)
         cv2.imwrite(path_to_photo, eroded)
 
-    def analyze_photo(self, enhance_photo=True):
+    def analyze_photo(self, enhance_photo=False):
         """
         Analyze photo with OCR and extract text
 
@@ -125,10 +126,69 @@ class PhotoScanner:
         if enhance_photo:
             self.enhance_photo(path_to_photo)
         print(f"[INFO] Scanning file {path_to_photo}")
-        return self.reader.readtext(path_to_photo)
+        return pytesseract.image_to_string(path_to_photo, config="-l por")
+
+
+class TextExtractor:
+    """
+    Class responsible for extracting text from photo and making it a dataframe
+    """
+    def __init__(self):
+        # TODO come up with the way to fill 'need' column
+        self.scanner = PhotoScanner()
+        self.products_dictionary = {'product': [],
+                                    'price': [],
+                                    'category': [],
+                                    'need': []}
+        # Temporary solution
+        # TODO MOVE TO A SEPARATE FILE LATER
+        self.lookup = {'banana': 'fruits',
+                       'cereja': 'fruits',
+                       'madalenas': 'sweet',
+                       'crem': 'yoghurt',
+                       'activia': 'yoghurt',
+                       'bif': 'yoghurt'}
+
+        # Temporary solution
+        # TODO MOVE TO A SEPARATE FILE LATER
+        self.service_values_to_clean = ['IVA DESCRICAO', 'VALOR']
+
+    # TODO Unite the words like BIF CREM ACTIVIA into 1
+    def parse_text(self):
+        """
+        Parse text from text image
+        :return: dictionary with parsed items
+        """
+        text_to_parse = self.scanner.analyze_photo()
+        items_to_parse = text_to_parse.split("\n")
+
+        with open("../scan_results/tesseract_output.txt", "w") as output_file:
+            output_file.write(text_to_parse)
+
+        cleansed_items = [item for item in items_to_parse if item != '']
+        cleansed_items = [item for item in cleansed_items if item not in self.service_values_to_clean]
+        for idx, item in enumerate(cleansed_items):
+            if 'TOTAL' in item:
+                break
+            item_split = item.split()
+            # bought item itself always at the second position
+            try:
+                if item_split[1].lower() in self.lookup:
+                    self.products_dictionary['product'].append(item_split[1].lower())
+                    self.products_dictionary['category'].append(self.lookup[item_split[1].lower()])
+                    # in the well detected photo price always comes last
+                    if "," in item_split[-1]:
+                        self.products_dictionary['price'].append(float(item_split[-1].replace(",", ".")))
+                    elif " X " in cleansed_items[idx + 1]:
+                        self.products_dictionary['price'].append(float(cleansed_items[idx + 1][-1].replace(",", ".")))
+                    else:
+                        print(
+                            "[WARNING] Try taking another photo and try again, this was not recognized correctly! Terminating...")
+                        raise Exception
+            except IndexError:
+                pass
 
 
 if __name__ == "__main__":
-    scaner = PhotoScanner()
-    result = scaner.analyze_photo()
-    print(result)
+    text_parser = TextExtractor()
+    text_parser.parse_text()
